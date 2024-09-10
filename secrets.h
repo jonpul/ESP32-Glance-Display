21bcf6868e1df8132b03775aed5dce1084307c84
jonpul <jonpul@msn.com>
2024-09-10 14:42:20 -0500

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -7,8 +7,4 @@ const char* FINNHUB_TOKEN = "<your token>";
 const char* WEATHER_TOKEN = "<your token>";   // weatherapi.com   
 const char* WEATHER_ZIP = "your weather zip"; // leave empty to use IP location for WeatherAPI.com
 const char* OPENWEATHER_TOKEN = "<your token>"; // openweather API
-// no longer needed due to new location check for weather
-//const char* OPENWEATHER_LAT = "<your latitude";
-//const char* OPENWEATHER_LONG = "<your longitude>";
-//const char* OPENWEATHER_CITY = "<your city name>";
-//const char* OPENWEATHER_REGION = "<your region/province/US State name>";
+
